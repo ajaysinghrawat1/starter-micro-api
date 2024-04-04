ba5bc7190230659b5f6acfaf2a26314ec8dd7442
ajayr <ajayrawat59941@gmail.com>
2024-04-04 22:19:20 +0530

    a

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     console.log(`Just got a request at ${req.url}!`)
-//     res.write('ajay rawat');
-//     res.end();
-// }).listen(process.env.PORT || 3000);
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`)
+    res.write('ajay rawat');
+    res.end();
+}).listen(process.env.PORT || 3000);
 
 
 
-const readline = require('readline');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-rl.question('Enter the first number: ', (num1) => {
-  rl.question('Enter the second number: ', (num2) => {
-    const result = parseFloat(num1) + parseFloat(num2);
-    console.log(`The sum of ${num1} and ${num2} is: ${result}`);
-    rl.close();
-  });
-});
+// rl.question('Enter the first number: ', (num1) => {
+//   rl.question('Enter the second number: ', (num2) => {
+//     const result = parseFloat(num1) + parseFloat(num2);
+//     console.log(`The sum of ${num1} and ${num2} is: ${result}`);
+//     rl.close();
+//   });
+// });
